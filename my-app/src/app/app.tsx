5a00eae8ac4d2795139fb7f34f0089ae870f6e31
Hernan Rinaldi <hernanjavierrinaldi@gmail.com>
2023-11-02 18:28:15 -0300

    modify files

---
+++ my-app/src/app/app.tsx
@@ -0,0 +1,12 @@
+
+// import { BrowserRouter as Router } from "react-router-dom";
+
+
+export default function app() {
+    return (
+      <main >
+        <h1>C </h1>
+        
+      </main>
+    )
+  } 
